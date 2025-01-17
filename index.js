fe9abde1c9ea7feec2193d3377d600d074c436b2
unknown <shohidulislamsifat2003@gmail.com>
2025-01-17 16:53:49 +0600

    creat session route done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var jwt = require('jsonwebtoken');
 const cors=require('cors')
 const express=require('express')
 const app=express()
@@ -24,12 +24,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const usercollection=client.db('studyweb').collection('user')
+const sessionCollection=client.db('studyweb').collection('allsession')
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/jwt',async(req,res)=>{
+  const user=req.body
+  const token=jwt.sign(user,process.env.ACESS_TOKEN_SECRET,{expiresIn:'1h'})
+  res.send({token})
+})
+
+app.post('/users',async(req,res)=>{
+  const user=req.body
+  const query={email:user.email}
+  const existinguser=await usercollection.findOne(query)
+  if(existinguser){
+    console.log('alredy in')
+    return res.send({message:'already in'})
+  }
+const result=await usercollection.insertOne(user)
+res.send(result)
+})
+// all session 
+app.post('/allsession',async(req,res)=>{
+  const sesstiondata=req.body
+  const result= await sessionCollection.insertOne(sesstiondata)
+  res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
