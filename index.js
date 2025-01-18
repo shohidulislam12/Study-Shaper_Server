f0c6639e5766ea352b2bf430bc4214d3ebf65e31
unknown <shohidulislamsifat2003@gmail.com>
2025-01-18 16:43:18 +0600

    get user and search

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/jwt',async(req,res)=>{
   const token=jwt.sign(user,process.env.ACESS_TOKEN_SECRET,{expiresIn:'1h'})
   res.send({token})
 })
-
+//all users
 app.post('/users',async(req,res)=>{
   const user=req.body
   const query={email:user.email}
@@ -57,10 +57,48 @@ app.post('/users',async(req,res)=>{
 const result=await usercollection.insertOne(user)
 res.send(result)
 })
+//all useres get 
+app.get('/users',async(req,res)=>{
+  const search=req.query.search
+  console.log(search)
+
+  let query={
+    $or: [
+      {name:{$regex:search,$options:'i'}},
+      {email:{$regex:search,$options:'i'}}
+    ]
+  }
+  const result=await usercollection.find(query).toArray()
+  res.send(result)
+})
+//update useres 
+app.patch('/updateuserole/:email',async(req,res)=>{
+const email=req.params.email
+const {role}=req.body
+const query={email}
+const updateDoc={
+  $set:{role:role}
+}
+console.log(role)
+  const result=await usercollection.updateOne(query,updateDoc)
+  res.send(result)
+})
 // all session 
 app.post('/allsession',async(req,res)=>{
   const sesstiondata=req.body
   const result= await sessionCollection.insertOne(sesstiondata)
+  res.send(result)
+})
+app.get('/allsession',async(req,res)=>{
+  const result=await sessionCollection.find().toArray()
+  res.send(result)
+})
+app.get('/allsession/:email',async(req,res)=>{
+  const email=req.params.email
+  const query ={
+    tutorEmail:email
+    }
+  const result=await sessionCollection.find(query).toArray()
   res.send(result)
 })
 
