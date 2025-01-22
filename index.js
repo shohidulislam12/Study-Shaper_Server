9e13add95c12cfb6c4ff8d267e3da9920bfaf264
unknown <shohidulislamsifat2003@gmail.com>
2025-01-22 16:05:04 +0600

    complete token creation

--- index.js
+++ index.js
@@ -39,6 +39,52 @@ async function run() {
       });
       res.send({ token });
     });
+    //verify token midlewire 
+    const verifyToken=(req,res,next)=>{
+console.log('inside verify token',req.headers.authorization)
+if(!req.headers.authorization){
+  return res.status(401).send({message:'forbidden acess'})
+}
+const token=req.headers.authorization.split(' ')[1]
+if(!token){
+  return res.status(401).send({message:'forbidden acess'})
+}
+jwt.verify(token,process.env.ACESS_TOKEN_SECRET,(err,decoded)=>{
+  if(err){
+    return res.status(401).send({message:'forbidden acess'})
+  }
+  req.decoded=decoded
+  next()
+})
+    }
+    //verify admin 
+   const  verifyAdmin =async(req,res,next )=>{
+const email=req.decoded.email
+const query={email:email}
+const user=await  usercollection.findOne(query)
+const isAdmin=user?.role==='admin'
+if(!isAdmin){
+  return res.status(403).send({message:'forbidden access'})
+}
+next()
+   }
+
+    app.get('/user/admin/:email',verifyToken,async(req,res)=>{
+const email= req.params.email
+if(email!==req.decoded.email){
+  res.status(403).send({message:'forbidden acess'})
+}
+const query={email:email}
+const user=await usercollection.findOne(query)
+let admin=false
+if(user){
+  admin=user?.role==='admin'
+}
+res.send({admin})
+    })
+
+
+
     //all users
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -66,7 +112,7 @@ async function run() {
       res.send(result);
     });
     //update useres
-    app.patch("/updateuserole/:email", async (req, res) => {
+    app.patch("/updateuserole/:email",verifyToken,verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const { role } = req.body;
       const query = { email };
@@ -240,7 +286,8 @@ async function run() {
       console.log(result);
     });
     // get booking data by email
-    app.get("/bookingdata/:email", async (req, res) => {
+    app.get("/bookingdata/:email",verifyToken, async (req, res) => {
+   
       const email = req.params.email;
       const query = { studentEmail: email };
       const result = await bookedcollection.find(query).toArray();
